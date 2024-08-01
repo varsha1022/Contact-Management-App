d21fbdc8801e50d6c0020fbb235c65177e7d4881
varsha <varshagaddam30@gmail.com>
2024-08-01 14:07:30 +0530

    git commit -m "changes"

--- src/Components/ContactList.js
+++ src/Components/ContactList.js
@@ -8,21 +8,25 @@ const ContactList = (props) => {
     if (!contacts.length) {
         return (
             <div className="m-8 flex flex-col gap-7">
-                <h2 className="text-3xl ml-4 font-200 font-bold">Contact List</h2>
-                <Link to="/add">
-                    <button className="w-1/12 py-3 bg-blue-500 text-white font-semibold rounded hover:bg-blue-600 transition duration-200 tracking-wider mt-4">
-                        Add Contact
-                    </button>
-                </Link>
-                <div className="border-2 focus:border-blue-400 h-12 pl-3 w-5/12 border-gray-300 rounded-lg mt-4">
-                    <i className="bi bi-search shadow-sm ml-3 pt-2 text-xl"></i>
-                    <input
-                        type="text"
-                        className="w-64 h-10 pl-2 bg-transparent border-0 outline-0 pl-4 text-indigo-950"
-                        placeholder="Search your Contacts"
-                        value={term}
-                        onChange={(e) => searchKeyWord(e.target.value)}
-                    />
+                <div className="flex items-center justify-between">
+                    <h2 className="text-3xl ml-4 font-200 font-bold">Contact List</h2>
+                    <Link to="/add">
+                        <button className="py-3 bg-blue-500 text-white font-semibold rounded hover:bg-blue-600 transition duration-200 tracking-wider w-40">
+                            Add Contact
+                        </button>
+                    </Link>
+                </div>
+                <div className="flex mt-4">
+                    <div id="searchContainer" className="border-2 focus:border-blue-400 h-12 w-full border-gray-300 rounded-lg flex items-center">
+                        <i className="bi bi-search text-xl ml-3"></i>
+                        <input
+                            type="text"
+                            className="w-full h-10 pl-2 bg-transparent border-0 outline-0 pl-4 text-indigo-950"
+                            placeholder="Search your Contacts"
+                            value={term}
+                            onChange={(e) => searchKeyWord(e.target.value)}
+                        />
+                    </div>
                 </div>
                 <div className="flex flex-col gap-4 divide-y divide-slate-200 mt-4">
                     <p>No contacts available.</p>
@@ -42,24 +46,26 @@ const ContactList = (props) => {
 
     return (
         <div className="m-8 flex flex-col gap-7">
-            <div className="flex justify-between">
-            <h2 className="text-3xl ml-4 font-200 font-bold">Contact List</h2>
-            <Link to="/add">
-                <button className="w-12/12 p-2 bg-blue-600 text-white font-bold rounded hover:bg-blue-600 transition duration-200 tracking-wide h-12 text-lg ">
-                    Add Contact
-                </button>
-            </Link>
+            <div className="flex items-center justify-between">
+                <h2 className="text-3xl  font-200 font-bold">Contact List</h2>
+                <Link to="/add">
+                    <button className="py-4 bg-blue-600 text-white font-bold rounded hover:bg-blue-700 transition duration-200 tracking-wide text-xl w-48">
+                        Add Contact
+                    </button>
+                </Link>
             </div>
 
-            <div className=" w-full border-2 focus:border-blue-400 h-12 pl-3 w-5/12 border-gray-300 rounded-lg mt-4">
-                <i className="bi bi-search shadow-sm ml-3 pt-2 text-xl"></i>
-                <input
-                    type="text"
-                    className="w-64 h-10 pl-2 bg-transparent border-0 outline-0 pl-4 text-indigo-950"
-                    placeholder="Search your Contacts"
-                    value={term}
-                    onChange={(e) => searchKeyWord(e.target.value)}
-                />
+            <div className="w-full flex mt-4">
+                <div id="searchContainer" className="w-full border-2 focus:border-blue-400 h-12 border-gray-300 rounded-lg flex items-center">
+                    <i className="bi bi-search text-xl ml-3"></i>
+                    <input
+                        type="text"
+                        className="w-full h-10 pl-2 bg-transparent border-0 outline-0 pl-4 text-indigo-950"
+                        placeholder="Search your Contacts"
+                        value={term}
+                        onChange={(e) => searchKeyWord(e.target.value)}
+                    />
+                </div>
             </div>
             <div className="flex flex-col gap-4 divide-y divide-slate-200 mt-4">
                 {renderContactList}
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -4,12 +4,12 @@ import { Link } from 'react-router-dom';
 const Header = () => {
     return (
         <header className="fixed top-0 left-0 right-0 bg-blue-600 text-white p-4 ">
-            <div className=" container mx-auto flex  justify-center items-center">
+            <div className=" container mx-auto flex  justify-center items-center  ml-0">
                 <h1 className="text-3xl  font-bold ">Contact Manager</h1>
                 <nav>
                     <Link
                         to="/"
-                        className="ml-96 top-5 absolute right-10 font-bold text-white text-xl no-underline hover:text-gray-100"
+                        className=" top-6 absolute right-5 font-bold text-white text-xl no-underline hover:text-gray-100"
                         aria-label="Home"
                     >
                         Home
